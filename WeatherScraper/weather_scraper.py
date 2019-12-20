6d6a2adbb353a69f7f5ccbdb24ca729ab48aaf7e
Sam Thomas <samthomas@davidson.edu>
2019-12-20 13:45:55 -0600

    Updated weather_scraper so that it can track on several cities

--- WeatherScraper/weather_scraper.py
+++ WeatherScraper/weather_scraper.py
@@ -1,7 +1,5 @@
+import requests
 import urllib3
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-from selenium.common.exceptions import TimeoutException
 import certifi
 from bs4 import BeautifulSoup
 import json
@@ -18,7 +16,19 @@ License: BSD 2-clause
 This is a scraping program that creates a database that generates a CSV with
 the weather data from a specified city. It is currently set to Davidson, NC.
 """
-
+zip_codes = {
+    '07073':'EastRutherford','54304':'GreenBay','60605':'Chicago','02035':'Foxboro',
+    '48226':'Detroit','64129':'KansasCity','15212':'Pittsburgh','30313':'Atlanta',
+    '46225':'Indianapolis','76011':'Dallas','14217':'Buffalo','45202':'Cincinnati',
+    '94124':'SanFrancisco','85305':'Phoenix','63101':'StLouis','70112':'NewOrleans',
+    '28202':'Charlotte','44114':'Cleveland','98134':'Seattle','55415':'Minneapolis',
+    '19148':'Philadelphia','33607':'Tampa','77054':'Houston','33056':'Miami','92108':'SanDiego',
+    '32202':'Jacksonville','37213':'Nashville','21230':'Baltimore','80204':'Denver',
+    '94621':'Oakland','20785':'WashingtonDC','96818':'Halawa','44708':'Canton','02215':'Boston',
+    '10451':'Bronx','11368':'Queens','92806':'Anaheim','76011':'Arlington','53214':'Milwaukee',
+    '28036':'Davidson','45469':'Dayton','15282':'Duquesne','22030':'Fairfax','01003':'Amherst',
+    '02881':'Kingston','23173':'Richmond','14778':'StBonaventure'
+}
 agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
 
 def disableImages():
@@ -34,17 +44,15 @@ def disableImages():
     ## Set the modified profile while creating the browser object
     return firefoxProfile
 
-def get_weather1():
-    html = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-    r = html.request('POST', 'https://weather.com/weather/tenday/l/28036:4:US', headers=agent)
-    return BeautifulSoup(r.data, 'html.parser')
+def get_weather0(code):
+    r = requests.get('https://weather.com/weather/tenday/l/'+ code + ':4:US')
+    return BeautifulSoup(r.content, 'html.parser')
 
-def get_weather2(profile):
-    browser = webdriver.Firefox(profile)
-    browser.get('https://weather.com/weather/tenday/l/28036:4:US')
-    html = browser.execute_script('return document.body.innerHTML')
-    browser.close()
-    return BeautifulSoup(html, 'html.parser')
+def get_weather1(code):
+    global zip_codes
+    html = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+    r = html.request('POST', 'https://weather.com/weather/tenday/l/'+ code + ':4:US', headers=agent)
+    return BeautifulSoup(r.data, 'html.parser')
 
 def get_current_weather(soup):
     scripts = soup.find_all('script')
@@ -82,7 +90,8 @@ def get_forecasts(soup):
                 return data
 
 
-def update_csv(days):
+def update_csv(days, city):
+    global zip_codes
     fields = []
 
     fields.append(datetime.datetime.now())
@@ -92,39 +101,34 @@ def update_csv(days):
         fields.append(j["day"]["temperature"])
         fields.append(j["day"]["precipPct"])
 
-    with open('davidson_weather_data.csv', 'a') as f:
+    with open(city + '_weather_data.csv', 'a') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
 
 def job():
-    while True:
-        try:
-            print "Trying to pull weather remotely...."
-            soup = get_weather1()
-            break
-        except Exception:
-            print "Failed...."
-            continue
-
-    if 'Access Denied' in soup.find('title').string:
-        profile = disableImages()
+    global zip_codes
+    for code in zip_codes.keys():
         while True:
-            try:
-                print "Trying to pull weather with Selenium...."
-                soup = get_weather2(profile)
+            soup = get_weather0(code)
+            if 'Access Denied' not in soup.find('title').string:
                 break
-            except Exception:
-                print "Failed, trying again...."
+
+            soup = get_weather1(code)
+            if 'Access Denied' not in soup.find('title').string:
+                break
+            else:
+                time.sleep(5)
                 continue
 
-    print "Got weather...."
-    days = get_forecasts(soup)
-    update_csv(days)
+        day = get_forecasts(soup)
+        update_csv(day, zip_codes[code])
 
 def main():
-    # with open('davidson_weather_data.csv', 'w') as f:
-    #     # writer = csv.writer(f)
-    #     # writer.writerow(['timestamp', 'day_1_temp', 'day_1_precip', 'day_2_temp', 'day_2_precip', 'day_3_temp', 'day_3_precip', 'day_4_temp', 'day_4_precip', 'day_5_temp', 'day_5_precip', 'day_6_temp', 'day_6_precip', 'day_7_temp', 'day_7_precip', 'day_8_temp', 'day_8_precip', 'day_9_temp', 'day_9_precip', 'day_10_temp', 'day_10_precip'])
+    global zip_codes
+    for city in zip_codes.values():
+        with open(city + '_weather_data.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(['timestamp', 'day_1_temp', 'day_1_precip', 'day_2_temp', 'day_2_precip', 'day_3_temp', 'day_3_precip', 'day_4_temp', 'day_4_precip', 'day_5_temp', 'day_5_precip', 'day_6_temp', 'day_6_precip', 'day_7_temp', 'day_7_precip', 'day_8_temp', 'day_8_precip', 'day_9_temp', 'day_9_precip', 'day_10_temp', 'day_10_precip'])
 
     schedule.every().day.at("00:00").do(job)
 
